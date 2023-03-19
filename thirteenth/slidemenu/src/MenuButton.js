54768bae94f731339cb829aa412b559832bb6c9c
Ruslan <115349782+khesea@users.noreply.github.com>
2023-03-19 17:41:15 +0300

    Update MenuButton.js

--- thirteenth/slidemenu/src/MenuButton.js
+++ thirteenth/slidemenu/src/MenuButton.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 import './MenuButton.css';
 
 class MenuButton extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return false;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (nextProps.handleMouseDown === this.props.handleMouseDown) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
 
   render() {
     console.log("Rendering: MenuButton");
