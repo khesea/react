0f7f0702ef6310439fe057f31b9a74ef23f06426
Ruslan <115349782+khesea@users.noreply.github.com>
2023-03-19 17:41:54 +0300

    Update MenuButton.js

--- thirteenth/slidemenu/src/MenuButton.js
+++ thirteenth/slidemenu/src/MenuButton.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import './MenuButton.css';
 
-class MenuButton extends Component {
+class MenuButton extends PureComponent {
   // shouldComponentUpdate(nextProps, nextState) {
   //   if (nextProps.handleMouseDown === this.props.handleMouseDown) {
   //     return false;
