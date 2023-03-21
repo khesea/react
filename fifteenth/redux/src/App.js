17d322a5414b35cc6c4e2819790bcf42cb1394d3
Ruslan <115349782+khesea@users.noreply.github.com>
2023-03-22 04:04:48 +0500

    files

---
+++ fifteenth/redux/src/App.js
@@ -0,0 +1,35 @@
+import {connect} from "react-redux";
+import Counter from "./Counter";
+
+
+//Сопоставление состояния Redux со свойствами компонента
+function mapStateToProps(state) {
+  return {
+    countValue: state.count
+  };
+}
+
+
+//Действие
+var increaseAction = {type: "increase"};
+var decreaseAction = {type: "decrease"};
+
+//Сопоставление действий Redux со свойствами компонента
+function mapDispatchToProps(dispatch) {
+  return {
+    increaseCount: function() {
+      return dispatch(increaseAction);
+    },
+    decreaseCount: function() {
+      return dispatch(decreaseAction);
+    }
+  };
+}
+
+//НОС-компонент
+var connectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+) (Counter);
+
+export default connectedComponent;
